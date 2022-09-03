15411dc9fca34e0fc166bcca44cc271c895027f8
VictorJhepherson <54046784+VictorJhepherson@users.noreply.github.com>
2022-09-03 15:52:01 -0300

    03.09.22 - Victor - Users and Models

--- src/database/models/users/index.ts
+++ src/database/models/users/index.ts
@@ -1,10 +1,20 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema, SchemaTypes } from 'mongoose';
+import { IUser } from '../../../interfaces';
 
-const userSchema = new Schema(
+const userSchema: Schema<IUser> = new Schema(
   {
-    username: {
+    firstName: {
       type: String,
       required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    nickName: {
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
       type: String,
@@ -15,15 +25,33 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    favorites: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+    lists: {
+      type: SchemaTypes.Mixed,
+      required: false,
+      default: {
+        nextWatch: [],
+        completed: [],
+        inProgress: [],
+        dropped: [],
+        topTen: [],
+      },
+    },
   },
   {
     timestamps: {
       createdAt: 'created_at',
       updatedAt: 'updated_at',
     },
+    versionKey: false,
   },
 );
 
-const User = model('User', userSchema);
+const User = model<IUser>('User', userSchema);
 
 export default User;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './response';
 export * from './validation';
 export * from './errors';
+export * from './users';
